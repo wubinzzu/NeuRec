f3104f65612100e54e756124c8b665249c9b7807
Jonathan Staniforth <jonathanstaniforth@gmail.com>
2019-10-03 15:28:10 +0800

    Added bool helper & corrected list helper with conversion type as well

--- neurec/util/helpers.py
+++ neurec/util/helpers.py
@@ -1,8 +1,33 @@
-def to_list(string, cast=float):
+import numpy
+import logging
+
+logger = logging.getLogger("neurec.util.helpers")
+
+def to_list(string):
     """Returns a list from a string.
 
-    string -- string in format [0.5,0.5]
+    string -- string in format [0.5,0.5]|type
     """
-    list = string[1:-1].split(",")
+    pipe_split = string.split("|")
 
-    return [cast(item) for item in list]
+    try:
+        cast = pipe_split[1]
+        array = pipe_split[0]
+        array_values = array[1:-1].split(",")
+    except IndexError:
+        logger.error("list index out of range for " + str(string) + ". Make sure the list is the form [0,0]|type")
+        raise
+
+    return numpy.array(array_values, dtype=cast)
+
+def to_bool(string):
+    """Returns a boolean from a string.
+
+    string -- string in format yes|no|true|false
+    """
+    if string.lower() in ("yes", "y", "true", "1"):
+        return True
+    elif string.lower() in ("no", "n", "false", "0"):
+        return False
+
+    raise ValueError(str(string) + ' not in ["yes", "y", "true", "1", "no", "n", "false", "0"].')

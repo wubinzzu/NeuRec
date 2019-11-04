4bf1c41d86e7dfc36693a511a4cb3c55da9852c9
Jonathan Staniforth <jonathanstaniforth@gmail.com>
2019-11-04 13:39:57 +0800

    Adjustments for publishing python package
    - Updated dockerfiles with neurec python package
    - Updated docs with docker image location
    - Add manifest file to add datasets to python package
    - Added init file for modules
    - Updated setup file with necessary details

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ import setuptools
 
 setuptools.setup(
     name="neurec",
-    version="0.0.1",
+    version="1.0.0",
     author="Jonathan Staniforth",
-    description="jonathanstaniforth@gmail.com",
-    long_description="",
+    author_email="jonathanstaniforth@gmail.com",
+    description="An open source neural recommender library",
+    long_description="NeuRec is a comprehensive and flexible Python library for recommendation models that includes a large range of state-of-the-art neural recommender models. This library aims to solve general and sequential (i.e. next-item) recommendation tasks, using the Tensorflow library to provide 26 models out of the box. NeuRec is open source and available under the MIT license. View the GitHub repository for more information: https://github.com/wubinzzu/NeuRec.git",
     url="https://github.com/wubinzzu/NeuRec.git",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='==3.6.8'
+    python_requires='>=3.5'
 )

245249b975378e284f71892f1eeae9a7f396518d
Zhongchuan Sun <iezcsun@gs.zzu.edu.cn>
2019-12-20 17:13:06 +0800

    Update SASRec

--- model/sequential_recommender/SASRec.py
+++ model/sequential_recommender/SASRec.py
@@ -16,8 +16,11 @@ from util import pad_sequences
 from util.Logger import logger
 
 
-def normalize(inputs, epsilon=1e-8, scope="ln", reuse=None):
-    """Applies layer normalization.
+def normalize(inputs,
+              epsilon=1e-8,
+              scope="ln",
+              reuse=None):
+    '''Applies layer normalization.
 
     Args:
       inputs: A tensor with 2 or more dimensions, where the first dimension has
@@ -29,7 +32,7 @@ def normalize(inputs, epsilon=1e-8, scope="ln", reuse=None):
 
     Returns:
       A tensor with the same shape and data dtype as `inputs`.
-    """
+    '''
     with tf.variable_scope(scope, reuse=reuse):
         inputs_shape = inputs.get_shape()
         params_shape = inputs_shape[-1:]
@@ -43,9 +46,101 @@ def normalize(inputs, epsilon=1e-8, scope="ln", reuse=None):
     return outputs
 
 
-def multihead_attention(queries, keys, num_units=None, num_heads=8, dropout_rate=0, is_training=True,
-                        causality=False, scope="multihead_attention", reuse=None, with_qk=False):
-    """Applies multihead attention.
+def embedding(inputs,
+              vocab_size,
+              num_units,
+              zero_pad=True,
+              scale=True,
+              l2_reg=0.0,
+              scope="embedding",
+              with_t=False,
+              reuse=None):
+    '''Embeds a given tensor.
+
+    Args:
+      inputs: A `Tensor` with type `int32` or `int64` containing the ids
+         to be looked up in `lookup table`.
+      vocab_size: An int. Vocabulary size.
+      num_units: An int. Number of embedding hidden units.
+      zero_pad: A boolean. If True, all the values of the fist row (id 0)
+        should be constant zeros.
+      scale: A boolean. If True. the outputs is multiplied by sqrt num_units.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A `Tensor` with one more rank than inputs's. The last dimensionality
+        should be `num_units`.
+
+    For example,
+
+    ```
+    import tensorflow as tf
+
+    inputs = tf.to_int32(tf.reshape(tf.range(2*3), (2, 3)))
+    outputs = embedding(inputs, 6, 2, zero_pad=True)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print sess.run(outputs)
+    >>
+    [[[ 0.          0.        ]
+      [ 0.09754146  0.67385566]
+      [ 0.37864095 -0.35689294]]
+
+     [[-1.01329422 -1.09939694]
+      [ 0.7521342   0.38203377]
+      [-0.04973143 -0.06210355]]]
+    ```
+
+    ```
+    import tensorflow as tf
+
+    inputs = tf.to_int32(tf.reshape(tf.range(2*3), (2, 3)))
+    outputs = embedding(inputs, 6, 2, zero_pad=False)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print sess.run(outputs)
+    >>
+    [[[-0.19172323 -0.39159766]
+      [-0.43212751 -0.66207761]
+      [ 1.03452027 -0.26704335]]
+
+     [[-0.11634696 -0.35983452]
+      [ 0.50208133  0.53509563]
+      [ 1.22204471 -0.96587461]]]
+    ```
+    '''
+    with tf.variable_scope(scope, reuse=reuse):
+        lookup_table = tf.get_variable('lookup_table',
+                                       dtype=tf.float32,
+                                       shape=[vocab_size, num_units],
+                                       # initializer=tf.contrib.layers.xavier_initializer(),
+                                       regularizer=tf.contrib.layers.l2_regularizer(l2_reg))
+        if zero_pad:
+            lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
+                                      lookup_table[1:, :]), 0)
+        outputs = tf.nn.embedding_lookup(lookup_table, inputs)
+
+        if scale:
+            outputs = outputs * (num_units ** 0.5)
+    if with_t:
+        return outputs, lookup_table
+    else:
+        return outputs
+
+
+def multihead_attention(queries,
+                        keys,
+                        num_units=None,
+                        num_heads=8,
+                        dropout_rate=0,
+                        is_training=True,
+                        causality=False,
+                        scope="multihead_attention",
+                        reuse=None,
+                        with_qk=False):
+    '''Applies multihead attention.
 
     Args:
       queries: A 3d tensor with shape of [N, T_q, C_q].
@@ -61,13 +156,16 @@ def multihead_attention(queries, keys, num_units=None, num_heads=8, dropout_rate
 
     Returns
       A 3d tensor with shape of (N, T_q, C)
-    """
+    '''
     with tf.variable_scope(scope, reuse=reuse):
         # Set the fall back option for num_units
         if num_units is None:
             num_units = queries.get_shape().as_list[-1]
 
         # Linear projections
+        # Q = tf.layers.dense(queries, num_units, activation=tf.nn.relu) # (N, T_q, C)
+        # K = tf.layers.dense(keys, num_units, activation=tf.nn.relu) # (N, T_k, C)
+        # V = tf.layers.dense(keys, num_units, activation=tf.nn.relu) # (N, T_k, C)
         Q = tf.layers.dense(queries, num_units, activation=None)  # (N, T_q, C)
         K = tf.layers.dense(keys, num_units, activation=None)  # (N, T_k, C)
         V = tf.layers.dense(keys, num_units, activation=None)  # (N, T_k, C)
@@ -121,15 +219,22 @@ def multihead_attention(queries, keys, num_units=None, num_heads=8, dropout_rate
         # Residual connection
         outputs += queries
 
+        # Normalize
+        # outputs = normalize(outputs) # (N, T_q, C)
+
     if with_qk:
         return Q, K
     else:
         return outputs
 
 
-def feedforward(inputs, num_units, scope="multihead_attention",
-                dropout_rate=0.2, is_training=True, reuse=None):
-    """Point-wise feed forward net.
+def feedforward(inputs,
+                num_units=[2048, 512],
+                scope="multihead_attention",
+                dropout_rate=0.2,
+                is_training=True,
+                reuse=None):
+    '''Point-wise feed forward net.
 
     Args:
       inputs: A 3d tensor with shape of [N, T, C].
@@ -140,7 +245,7 @@ def feedforward(inputs, num_units, scope="multihead_attention",
 
     Returns:
       A 3d tensor with the same shape and dtype as inputs
-    """
+    '''
     with tf.variable_scope(scope, reuse=reuse):
         # Inner layer
         params = {"inputs": inputs, "filters": num_units[0], "kernel_size": 1,
@@ -155,6 +260,9 @@ def feedforward(inputs, num_units, scope="multihead_attention",
 
         # Residual connection
         outputs += inputs
+
+        # Normalize
+        # outputs = normalize(outputs)
 
     return outputs
 
@@ -187,13 +295,15 @@ class SASRec(SeqAbstractRecommender):
         self.item_pos_ph = tf.placeholder(tf.int32, [None, self.max_len], name="item_pos")
         self.item_neg_ph = tf.placeholder(tf.int32, [None, self.max_len], name="item_neg")
         self.is_training = tf.placeholder(tf.bool, name="training_flag")
+
         l2_regularizer = tf.contrib.layers.l2_regularizer(self.l2_emb)
         item_embeddings = tf.get_variable('item_embeddings', dtype=tf.float32,
                                           shape=[self.items_num, self.hidden_units],
                                           regularizer=l2_regularizer)
 
         zero_pad = tf.zeros([1, self.hidden_units], name="padding")
-        self.item_embeddings = tf.concat([item_embeddings, zero_pad], axis=0)
+        item_embeddings = tf.concat([item_embeddings, zero_pad], axis=0)
+        self.item_embeddings = item_embeddings * (self.hidden_units ** 0.5)
 
         self.position_embeddings = tf.get_variable('position_embeddings', dtype=tf.float32,
                                                    shape=[self.max_len, self.hidden_units],
@@ -201,27 +311,35 @@ class SASRec(SeqAbstractRecommender):
 
     def build_graph(self):
         self._create_variable()
-        # embedding layer
-        batch_size = tf.shape(self.item_seq_ph)[0]
-        item_seq_emb = tf.nn.embedding_lookup(self.item_embeddings, self.item_seq_ph)
+        reuse = None
+        with tf.variable_scope("SASRec", reuse=reuse):
+            # sequence embedding, item embedding table
+            self.seq = tf.nn.embedding_lookup(self.item_embeddings, self.item_seq_ph)
+            item_emb_table = self.item_embeddings
 
-        position = tf.tile(tf.expand_dims(tf.range(self.max_len), 0), [batch_size, 1])
-        position_emb = tf.nn.embedding_lookup(self.position_embeddings, position)
+            # Positional Encoding
+            position = tf.tile(tf.expand_dims(tf.range(tf.shape(self.item_seq_ph)[1]), 0),
+                               [tf.shape(self.item_seq_ph)[0], 1])
+            t = tf.nn.embedding_lookup(self.position_embeddings, position)
+            # pos_emb_table = self.position_embeddings
 
-        item_seq_emb = item_seq_emb + position_emb
+            self.seq += t
 
-        # dropout and mask
-        item_seq_emb = tf.layers.dropout(item_seq_emb, rate=self.dropout_rate, training=self.is_training)
-        # the id of padding items is 'items_num'
-        mask = tf.expand_dims(tf.to_float(tf.not_equal(self.item_seq_ph, self.items_num)), -1)  # (b, l, 1)
-        item_seq_emb = tf.multiply(item_seq_emb, mask)
+            # Dropout
+            self.seq = tf.layers.dropout(self.seq,
+                                         rate=self.dropout_rate,
+                                         training=tf.convert_to_tensor(self.is_training))
 
-        # build blocks
-        for i in range(self.num_blocks):
-            with tf.variable_scope("num_blocks_%d" % i):
-                # Self-attention
-                item_seq_emb = multihead_attention(queries=normalize(item_seq_emb),
-                                                   keys=item_seq_emb,
+            mask = tf.expand_dims(tf.to_float(tf.not_equal(self.item_seq_ph, self.items_num)), -1)
+            self.seq *= mask
+
+            # Build blocks
+
+            for i in range(self.num_blocks):
+                with tf.variable_scope("num_blocks_%d" % i):
+                    # Self-attention
+                    self.seq = multihead_attention(queries=normalize(self.seq),
+                                                   keys=self.seq,
                                                    num_units=self.hidden_units,
                                                    num_heads=self.num_heads,
                                                    dropout_rate=self.dropout_rate,
@@ -229,44 +347,44 @@ class SASRec(SeqAbstractRecommender):
                                                    causality=True,
                                                    scope="self_attention")
 
-                # feed forward
-                item_seq_emb = feedforward(normalize(item_seq_emb), num_units=[self.hidden_units, self.hidden_units],
-                                           dropout_rate=self.dropout_rate, is_training=self.is_training)
+                    # Feed forward
+                    self.seq = feedforward(normalize(self.seq),
+                                           num_units=[self.hidden_units, self.hidden_units],
+                                           dropout_rate=self.dropout_rate,
+                                           is_training=self.is_training)
+                    self.seq *= mask
 
-                item_seq_emb = tf.multiply(item_seq_emb, mask)
+            self.seq = normalize(self.seq)  # (b, l, d)
+            last_emb = self.seq[:, -1, :]  # (b, d), the embedding of last item of each session
 
-        item_seq_emb = normalize(item_seq_emb)  # (b, l, d)
+        pos = tf.reshape(self.item_pos_ph, [tf.shape(self.item_seq_ph)[0] * self.max_len])  # (b*l,)
+        neg = tf.reshape(self.item_neg_ph, [tf.shape(self.item_seq_ph)[0] * self.max_len])  # (b*l,)
+        pos_emb = tf.nn.embedding_lookup(item_emb_table, pos)  # (b*l, d)
+        neg_emb = tf.nn.embedding_lookup(item_emb_table, neg)  # (b*l, d)
+        seq_emb = tf.reshape(self.seq, [tf.shape(self.item_seq_ph)[0] * self.max_len, self.hidden_units])  # (b*l, d)
 
-        item_pos = tf.reshape(self.item_pos_ph, [batch_size * self.max_len])  # (b*l,)
-        item_neg = tf.reshape(self.item_neg_ph, [batch_size * self.max_len])  # (b*l,)
+        # prediction layer
+        self.pos_logits = inner_product(pos_emb, seq_emb)  # (b*l,)
+        self.neg_logits = inner_product(neg_emb, seq_emb)  # (b*l,)
 
-        pos_emb = tf.nn.embedding_lookup(self.item_embeddings, item_pos)  # (b*l, d)
-        neg_emb = tf.nn.embedding_lookup(self.item_embeddings, item_neg)  # (b*l, d)
-        seq_emb = tf.reshape(item_seq_emb, [batch_size * self.max_len, self.hidden_units])  # (b*l, d)
+        # ignore padding items (self.items_num)
+        is_target = tf.reshape(tf.to_float(tf.not_equal(pos, self.items_num)),
+                               [tf.shape(self.item_seq_ph)[0] * self.max_len])
 
-        # rating calculation
-        pos_logits = inner_product(pos_emb, seq_emb)  # (b*l,)
-        neg_logits = inner_product(neg_emb, seq_emb)  # (b*l,)
+        pos_loss = -tf.log(tf.sigmoid(self.pos_logits) + 1e-24) * is_target
+        neg_loss = -tf.log(1 - tf.sigmoid(self.neg_logits) + 1e-24) * is_target
+        self.loss = tf.reduce_sum(pos_loss + neg_loss) / tf.reduce_sum(is_target)
 
-        # loss calculation
-        # ignore padding items (0)
-        target_mask = tf.to_float(tf.not_equal(self.item_pos_ph, self.items_num))  # (b, l)
-        target_mask = tf.reshape(target_mask, [batch_size*self.max_len])  # (b*l,)
-
-        pos_loss = -tf.log(tf.sigmoid(pos_logits) + 1e-24) * target_mask
-        neg_loss = -tf.log(1 - tf.sigmoid(neg_logits) + 1e-24) * target_mask
-        loss = tf.reduce_sum(pos_loss+neg_loss) / tf.reduce_sum(target_mask)
         try:
             reg_losses = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-            loss = loss + reg_losses
+            self.loss = self.loss + reg_losses
         except:
             pass
 
-        self.train_opt = tf.train.AdamOptimizer(learning_rate=self.lr, beta2=0.98).minimize(loss)
+        self.train_opt = tf.train.AdamOptimizer(learning_rate=self.lr, beta2=0.98).minimize(self.loss)
 
         # for predication/test
-        items_embeddings = self.item_embeddings[:-1]  # remove the padding item
-        last_emb = item_seq_emb[:, -1, :]  # (b, d), the embedding of last item of each session
+        items_embeddings = item_emb_table[:-1]  # remove the padding item
         self.all_logits = tf.matmul(last_emb, items_embeddings, transpose_b=True)
 
     def train_model(self):
